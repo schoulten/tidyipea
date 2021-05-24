310c012893f055ebaf37eed16debd67d15ca6269
Fernando da Silva <fernando.schoulten@hotmail.com>
2021-05-24 13:36:01 -0300

    Initial commit

---
+++ R/codes_ipea.R
@@ -0,0 +1,88 @@
+#' Get vailable series from IPEADATA
+#'
+#' @param language 	String specifying the selected language. Language options are English ("en", default) and Brazilian portuguese ("br").
+#'
+#' @return Tibble containing Ipeadata code, name, theme, source, frequency, last update and activity status of available series.
+#' @description Returns a tibble with available series from IPEADATA'S API.
+#' @encoding UTF-8
+#' @export
+#'
+#' @examples
+#' all_codes <- codes_ipea()
+codes_ipea <- function(language = c("en", "br")) { #f unction from IPEADATAR with MIT license
+
+  # Check language arg
+  language <- match.arg(language)
+
+  # URL for metadata
+  url <- 'http://www.ipeadata.gov.br/api/odata4/Metadados/'
+
+  # Get data
+  series <- jsonlite::fromJSON(url, flatten = TRUE)[[2]] %>%
+    dplyr::as_tibble() %>%
+    dplyr::select(
+      c(
+        'SERCODIGO', 'SERNOME', 'BASNOME', 'FNTSIGLA',
+        'PERNOME', 'SERATUALIZACAO', 'SERSTATUS'
+        )
+      ) %>%
+    dplyr::mutate(
+      FNTSIGLA       = as.factor(FNTSIGLA),
+      SERATUALIZACAO = lubridate::as_date(SERATUALIZACAO),
+      SERSTATUS      = as.character(SERSTATUS) %>%
+        dplyr::if_else(is.na(.), '', .)
+      )
+
+  # Setting labels in selected language
+  if (language == 'en') {
+
+    series <- series %>%
+      dplyr::mutate(
+        SERSTATUS = factor(
+          SERSTATUS,
+          levels = c('A', 'I', ''),
+          labels =  c('Active', 'Inactive', '')
+          ),
+        PERNOME = iconv(PERNOME, 'UTF-8', 'ASCII//TRANSLIT') %>%
+          factor(
+            levels = c(
+              'Anual', 'Decenal', 'Diaria', 'Irregular',
+              'Mensal', 'Quadrienal', 'Quinquenal',
+              'Semestral', 'Trimestral', 'Nao se aplica'
+              ),
+            labels = c(
+              'Yearly', 'Decennial', 'Daily', 'Irregular',
+              'Monthly', 'Quadrennial', 'Quinquennial',
+              'Semiannual', 'Quarterly', 'Not applicable'
+              )
+            ),
+        BASNOME = iconv(BASNOME, 'UTF-8', 'ASCII//TRANSLIT') %>%
+          factor(
+            levels = c('Macroeconomico', 'Regional', 'Social'),
+            labels = c('Macroeconomic', 'Regional', 'Social')
+            )
+        ) %>%
+      purrr::set_names(
+        c('code', 'name', 'theme', 'source', 'freq', 'lastupdate', 'status')
+        )
+
+  } else {
+
+    series <- series
+      dplyr::mutate(
+        SERSTATUS = factor(
+          SERSTATUS,
+          levels = c('A', 'I', ''),
+          labels =  c('Ativa', 'Inativa', '')
+          ),
+        BASNOME = factor(BASNOME),
+        PERNOME = factor(PERNOME)
+        ) %>%
+      purrr::set_names(
+        c('code', 'name', 'theme', 'source', 'freq', 'lastupdate', 'status')
+        )
+
+  }
+
+  return(series)
+}
---
+++ R/get_ipea.R
@@ -0,0 +1,75 @@
+#' Get IPEADATA series
+#'
+#' @param code Character vector with series code.
+#'
+#' @return A tibble with data from IPEADATA
+#' @description This function provides the extraction of data using IPADATA's API <http://www.ipeadata.gov.br/api/>.
+#' @details See the [codes_ipea()] to available series codes and other information.
+#' @encoding UTF-8
+#' @export
+#'
+#' @examples
+#' get_ipea(c("CAGED12_SALDON12", "CAGED12_DESLIGN12"))
+get_ipea <- function(code){
+
+  # Check if argument is character
+  if (purrr::is_character(code) & !purrr::is_null(code)) {
+    code <- toupper(code)
+  } else stop("Code argument must be a character, check the argument.", call. = FALSE)
+
+  # Ler página
+  raw_dados <- try(
+    suppressWarnings(
+      purrr::map(
+        .x = purrr::map(
+          .x = code,
+          ~paste0(
+            "http://www.ipeadata.gov.br/api/odata4/ValoresSerie(SERCODIGO='",
+            .x,
+            "')"
+          )
+        ),
+        ~jsonlite::fromJSON(.x)
+      )
+    ),
+    silent = TRUE
+  )
+
+  # Warning message for series with null return
+  if (0 %in% purrr::map(
+    .x = 1:length(code) %>% purrr::set_names(code),
+    ~length(raw_dados[[.x]][["value"]]))
+  ) {
+
+    flag <- purrr::map(
+      .x = 1:length(code) %>% purrr::set_names(code),
+      ~length(raw_dados[[.x]][["value"]])
+    ) %>%
+      dplyr::as_tibble() %>%
+      tidyr::pivot_longer(colnames(.)) %>%
+      dplyr::filter(value == 0)
+
+    warning(
+      paste0(
+        "Code [",
+        paste(flag$name, collapse = ", "),
+        "] not available / not found in IPEADATA."
+      ),
+      call. = FALSE
+    )
+
+  }
+
+  # Data wrangling
+  dados <- purrr::map(.x = 1:length(code), ~raw_dados[[.x]][["value"]]) %>%
+    dplyr::bind_rows() %>%
+    dplyr::select("date" = `VALDATA`, "value" = `VALVALOR`, "code" = `SERCODIGO`) %>%
+    dplyr::mutate(
+      date = lubridate::as_date(date),
+      code = as.factor(code)
+    ) %>%
+    dplyr::as_tibble()
+
+  return(dados)
+
+}
